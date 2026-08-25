f02dd2b6d976851f49cf0c7b89e41fcf54497aea
j4b4zao <37563309+j4b4zao@users.noreply.github.com>
2026-08-24 21:47:57 -0300

    Trader Psychology RC2: export engine contracts

--- psychology/__init__.py
+++ psychology/__init__.py
@@ -1,5 +1,17 @@
 """Camada observacional de Psicologia do Trader."""
 
+from psychology.trader_psychology_engine import (
+    TraderPsychologyEngine,
+    TraderPsychologyPolicy,
+    TraderPsychologyResult,
+    TraderPsychologySignal,
+)
 from psychology.trader_psychology_state import TraderPsychologyState
 
-__all__ = ["TraderPsychologyState"]
+__all__ = [
+    "TraderPsychologyEngine",
+    "TraderPsychologyPolicy",
+    "TraderPsychologyResult",
+    "TraderPsychologySignal",
+    "TraderPsychologyState",
+]
